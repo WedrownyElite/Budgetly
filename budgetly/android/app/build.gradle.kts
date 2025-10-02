8da0ebe0341d04ba1c1956786a3d3c841585a7be
WedrownyElite <chris.sliwinski@gmail.com>
2025-10-02 14:29:33 -0600

    Updated namespace to com.oddologyinc.budgetlyapp

--- budgetly/android/app/build.gradle.kts
+++ budgetly/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.budgetly"
+    namespace = "com.oddologyinc.budgetly"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.budgetly"
+        applicationId = "com.oddologyinc.budgetly"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- budgetly/android/app/src/main/kotlin/com/example/budgetly/MainActivity.kt
+++ budgetly/android/app/src/main/kotlin/com/example/budgetly/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.budgetly
+package com.oddologyinc.budgetly
 
 import io.flutter.embedding.android.FlutterActivity
 
