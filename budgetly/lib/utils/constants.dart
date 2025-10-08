bb31c4db7cbe7397affdf78d54d22c73258c4c55
WedrownyElite <chris.sliwinski@gmail.com>
2025-10-08 08:33:20 -0600

    Changed budgetly port

--- budgetly/lib/utils/constants.dart
+++ budgetly/lib/utils/constants.dart
@@ -1,4 +1,4 @@
 ﻿class AppConstants {
-  static const String backendUrl = 'https://budgetly.oddologyinc.com';
+  static const String backendUrl = 'https://budgetly.oddologyinc.com:3003';
   static const String accessTokenKey = 'plaid_access_token';
 }
